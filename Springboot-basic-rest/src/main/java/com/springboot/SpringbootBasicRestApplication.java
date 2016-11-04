b90bc19904a6ac756bed3ba60b7d3fdec45c98d9
rselvaraj <rselvaraj@altimetrik.com>
2016-11-04 16:17:13 +0530

    Spring boot rest

---
+++ Springboot-basic-rest/src/main/java/com/springboot/SpringbootBasicRestApplication.java
@@ -0,0 +1,12 @@
+package com.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBasicRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBasicRestApplication.class, args);
+	}
+}
